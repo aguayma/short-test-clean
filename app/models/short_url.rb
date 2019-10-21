e3cc2efc437f22a913ecc819a9ebf3b8b94bdfc9
aguayma <mario.aguayo0902@gmail.com>
2019-10-21 19:55:22 -0400

    Added url validation in ShortUrl model. Created regex variable to check to see if url passes. Will handle failure once url redirect tracking is integrated

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,8 +1,8 @@
 class ShortUrl < ApplicationRecord
-
+  URL_VALIDATION = /^(((http|https):\/\/|)?[a-z0-9]+\D+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(\/.*)?)$/
   CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
 
-  validate :validate_full_url
+  validate :validate_full_url, :on => :create
 
   def short_code
   end
@@ -13,6 +13,10 @@ class ShortUrl < ApplicationRecord
   private
 
   def validate_full_url
+    if !!url.match(URL_VALIDATION)
+    else
+      errors.add(:url, 'is not a valid url.')
+    end
   end
 
 end
--- db/migrate/20190923133432_create_short_urls.rb
+++ db/migrate/20190923133432_create_short_urls.rb
@@ -1,8 +1,8 @@
 class CreateShortUrls < ActiveRecord::Migration[6.0]
   def change
     create_table :short_urls do |t|
-      # You'll want to add some attributes here...
-
+      t.string :url
+      t.string :code
       t.timestamps
     end
   end
